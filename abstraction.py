dd9cf0bc9bd340d9f9c3ddcba4aad8f3ee6e7eef
jagadeesh81 <g.jagadeesh81@gmail.com>
2021-03-01 18:36:08 +0530

    adding oops concepts

---
+++ abstraction.py
@@ -0,0 +1,9 @@
+class Arjunamma:
+    def met(self):
+        print('in met method')
+    def __met1(self):
+        print('in met1 method')
+
+junk1=Arjunamma()
+junk1.met()
+junk1._Arjunamma__met1()
